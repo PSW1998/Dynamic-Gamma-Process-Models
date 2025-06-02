9646ca94e1ca32c7c5c6d1648cafe406aa86008a
PSW1998 <59308806+PSW1998@users.noreply.github.com>
2025-06-01 21:40:01 -0400

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,1 +1,12 @@
 # Here we will put the tests
+from dynagamma import square, greet
+
+def test_square_int():
+    assert square(3) == 9
+
+def test_square_float():
+    assert square(2.5) == 6.25
+
+def test_greet():
+    assert greet("Patrick") == "Hello, Patrick!"
+
