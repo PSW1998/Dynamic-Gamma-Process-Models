458812a9c15c676f0aa023038596bc8249fbe2ae
PSW1998 <59308806+PSW1998@users.noreply.github.com>
2025-06-01 21:39:28 -0400

    Update __init__.py

--- src/dynagamma/__init__.py
+++ src/dynagamma/__init__.py
@@ -1,1 +1,5 @@
 # Add package skeleton: src/dynagamma/init.py
+from .core import greet, square    # import it here so users can call dynagamma.square
+
+__all__: list[str] = ["greet", "square"]
+__version__ = "0.1.0"
